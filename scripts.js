13f626f80947f09c3d7d7cc65faf8ed3d27ff24f
Ender2014 <lau.zihao@gmail.com>
2024-10-06 19:06:54 +0800

    Minor adjustments

--- scripts.js
+++ scripts.js
@@ -1,25 +1,43 @@
-const GRID_NUM = 16*16;
+const INIT_GRID_NUM = 16*16;
 const container = document.querySelector(".container");
 
-// Initialize grid divs.
-for(let i = 0; i<GRID_NUM; i++){
-    let divGrid = document.createElement("div");
-    container.appendChild(divGrid);
+// Function to initialize grid divs.
+function initializeGrid(numOfGrids){
+    const numOfSides = Math.sqrt(numOfGrids);
+
+    for(let i = 0; i<numOfGrids; i++){
+        let divGrid = document.createElement("div");
+        divGrid.style.width = `calc((100% / ${numOfSides}))`;
+        divGrid.style.height = `calc((100% / ${numOfSides}))`;
+        container.appendChild(divGrid);
+    }
 }
 
 // Applies a pink hover effect to all grid items.
-const gridItems = document.querySelectorAll(".container div");
+function hoverGrid(){
+    const gridItems = document.querySelectorAll(".container div");
 
-gridItems.forEach((gridItem) =>{
-    gridItem.addEventListener("mouseenter", (e) => {
-        e.target.style.background = "pink";
+    gridItems.forEach((gridItem) =>{
+        gridItem.addEventListener("mouseenter", (e) => {
+            e.target.style.background = "pink";
+        });
     });
-});
+}
 
-// Prompt for user to set new size
+// Function to create new grid
+function createNewGrid(){ 
+    let size = (prompt("Number of squares per side?")) ** 2;
+    // Reset grid
+    container.replaceChildren()
+    // Apply back css
+    initializeGrid(size);
+}
+
+
+//Initialize grid/sktchpad.
+initializeGrid(INIT_GRID_NUM)
+
+// Prompt for user to set new size.
 const btn = document.querySelector(".header button");
 
-btn.addEventListener("click", () => {
-    let size = prompt("Number of squares per side?");
-    console.log(size);
-})
+btn.addEventListener("click", () => createNewGrid());
